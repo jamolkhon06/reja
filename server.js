34cb0df1508d9e0b44fa540495afeb3e9ea811bd
Joseph <j4880989@gmail.com>
2026-08-26 09:19:56 +0900

    BRR: build express web server

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+
+// 1: Kirish code
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// 2: Session code
+
+// 3: Views code
+app.set('views', 'views');
+app.set('view engine', 'ejs');
+// BSSR => Backend server side rendering
+
+// 4: Routing code
+app.post("/create-item", (req, res) => {
+    console.log(req.body);
+    res.json({ test: "success" })
+})
+
+app.get('/', (req, res) => {
+    res.render('harid')
+});
+
+const server = http.createServer(app);
+let PORT = 3000;
+server.listen(PORT, () => {
+    console.log(`The server is running successfully on port: ${PORT}`);
+})
