5a25a389531ab925fe3e986f7973da3e0fea4222
Joseph <j4880989@gmail.com>
2026-08-28 09:26:30 +0900

    completed NodeJS event loop va Callback functionlarni o'rganamiz lesson

--- train.js
+++ train.js
@@ -1,1 +1,32 @@
-console.log('Train ishga tushdi...')
+console.log('Jack Ma maslahatlari');
+const list = [
+    "yaxshi talaba bo'ling", // 0-20
+    "to'g'ri boshliq tanlang va ko'proq xato qiling", // 20-30
+    "o'zingizga ishlashingizni boshlang", // 30-40
+    "siz kuchli bo'lgan narsalarni qiling", // 40-50
+    "yoshlarga investitsiya qiling", // 50-60
+    "endi dam oling, foydasi yo'q endi", // 60
+];
+
+function maslahatBering(a, callback) {
+    if(typeof a !== "number") callback("please, insert number", null);
+    else if(a <= 20) callback(null, list[0]);
+    else if(a > 20 && a <= 30) callback(null, list[1]);
+    else if(a > 30 && a <= 40) callback(null, list[2]);
+    else if(a > 40 && a <= 50) callback(null, list[3]);
+    else if(a > 50 && a <= 60) callback(null, list[4]);
+    else {
+        setTimeout(() => {
+            callback(null, list[5]);
+        }, 5000)
+    }
+}
+
+console.log("Passed here 0")
+maslahatBering(10, (err, data) => {
+    if(err) console.error('ERROR', err);
+    else {
+        console.log('JAVOB:', data);
+    }
+})
+console.log("Passed here 1")
