662ce8dc2e346317982d3751fbf940f1e1f7c7cd
Joseph <j4880989@gmail.com>
2026-08-28 10:03:47 +0900

    feat: train callback hamda async functions

--- train.js
+++ train.js
@@ -8,7 +8,50 @@ const list = [
     "endi dam oling, foydasi yo'q endi", // 60
 ];
 
-function maslahatBering(a, callback) {
+// Asynchronous functions lesson
+async function maslahatBering(a) {
+    if(typeof a !== "number") throw new Error("Insert a number");
+    else if(a <= 20) return list[0];
+    else if(a > 20 && a <= 30) return list[1]
+    else if(a > 30 && a <= 40) return list[2]
+    else if(a > 40 && a <= 50) return list[3]
+    else if(a > 50 && a <= 60) return list[4]
+    else {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve(list[5]);
+            }, 5000)
+        }) 
+    }
+}
+
+// call via then/catch
+/* console.log('Passed here 0');
+maslahatBering(20)
+                  .then(data => {
+                    console.log('JAVOB:', data)
+                })
+                  .catch(err => {
+                    console.error("ERROR:", err)
+                });
+console.log('Passed here 1') */
+
+// call via async await
+async function run() {
+    let javob = await maslahatBering(65);
+    console.log(javob);
+
+    javob = await maslahatBering(31);
+    console.log(javob);
+
+    javob = await maslahatBering(41);
+    console.log(javob);
+}
+run()
+
+
+// NodeJS event loop and Callback functions lesson
+/* function maslahatBering(a, callback) {
     if(typeof a !== "number") callback("please, insert number", null);
     else if(a <= 20) callback(null, list[0]);
     else if(a > 20 && a <= 30) callback(null, list[1]);
@@ -29,4 +72,4 @@ maslahatBering(10, (err, data) => {
         console.log('JAVOB:', data);
     }
 })
-console.log("Passed here 1")
+console.log("Passed here 1") */
