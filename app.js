40ca353310ddf17b06375170bcebfcf939034912
Joseph <j4880989@gmail.com>
2026-08-31 10:25:44 +0900

    feat: FrontEnd JavaScript'ni qurdik

--- app.js
+++ app.js
@@ -30,14 +30,9 @@ app.set('view engine', 'ejs');
 // 4: Routing code
 app.post("/create-item", (req, res) => {
     console.log('User entered /create-item');
-    const new_reja = req.body.item;
+    const new_reja = req.body.reja;
     db.collection("plans").insertOne({reja: new_reja}, (err, data) => {
-        if(err) {
-            console.error(err);
-            res.end("Something went wrong")
-        } else {
-            res.end("Successfully added")
-        }
+        res.json(data.ops[0])
     });
 })
 
